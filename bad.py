11cbefe342bba3b776bd427dbc511912f8e6acc2
Sabri <83362107+Sabrj@users.noreply.github.com>
2023-04-05 14:08:20 +0200

    Update bad.py

--- bad.py
+++ bad.py
@@ -1,8 +1,8 @@
 AWS_CREDENTIALS = { 'key': 'djekjdkejde', 'secret': 'dncndmncdmncd' }
 hashed_password = hashlib.md5(request.params['foo']).hexdigest()
 
-github_token2 = 
+github_token2 = "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"
 
-test = 
+test = "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"
 
-test2 = 
+test2 = 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
