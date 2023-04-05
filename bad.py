d73263a63cef6fc4679f46ff8fdeb10878bd88a8
Sabri <83362107+Sabrj@users.noreply.github.com>
2023-04-05 13:58:32 +0200

    Update bad.py

--- bad.py
+++ bad.py
@@ -1,2 +1,6 @@
 AWS_CREDENTIALS = { 'key': 'djekjdkejde', 'secret': 'dncndmncdmncd' }
 hashed_password = hashlib.md5(request.params['foo']).hexdigest()
+
+test = "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"
+
+test2 = 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
