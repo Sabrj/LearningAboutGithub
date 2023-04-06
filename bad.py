f09c0e250a93b58680aafd15b8269099b17054c9
Sabri <83362107+Sabrj@users.noreply.github.com>
2023-04-06 06:56:11 +0200

    Update bad.py

--- bad.py
+++ bad.py
@@ -3,4 +3,4 @@ hashed_password = hashlib.md5(request.params['foo']).hexdigest()
 
 test = "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"
 
-test2 = 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
+test2 = "eyJz9sdfsdfsdfsd"
