02454989d4dd877909d4569191aac0a4443f9262
Sabri <83362107+Sabrj@users.noreply.github.com>
2023-04-06 07:41:59 +0200

    Delete bad.py

--- bad.py
+++
@@ -1,6 +0,0 @@
-AWS_CREDENTIALS = { 'key': 'djekjdkejde', 'secret': 'dncndmncdmncd' }
-hashed_password = hashlib.md5(request.params['foo']).hexdigest()
-
-test = "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"
-
-test2 = dmcxd329ujdmkemkd349r"
