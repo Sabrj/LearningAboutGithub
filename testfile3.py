39f3ce5b9420bc1bf81181960525c44a95d73aae
Sabri <83362107+Sabrj@users.noreply.github.com>
2023-04-06 08:51:53 +0200

    Update testfile3.py

--- testfile3.py
+++ testfile3.py
@@ -1,1 +1,12 @@
-hello world
+var config = require("../config"),
+    pgp = require('pg-promise')();
+
+function do_auth(username, password) {
+    var db = pgp(config.db.connectionString);
+
+    var q = "SELECT * FROM users WHERE name = '" + username + "' AND password ='" + password + "';";
+
+    return db.one(q);
+}
+
+module.exports = do_auth;
