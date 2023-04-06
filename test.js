96286624cc6d6ca203b2c701f026f49fd0e094b0
Sabri <83362107+Sabrj@users.noreply.github.com>
2023-04-06 07:58:12 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-var express = require('test');
+var config = require("../config"),
+    pgp = require('pg-promise')();
+
+function do_auth(username, password) {
+    var db = pgp(config.db.connectionString);
+
+    var q = "SELECT * FROM users WHERE name = '" + username + "' AND password ='" + password + "';";
+
+    return db.one(q);
+}
+
+module.exports = do_auth;
